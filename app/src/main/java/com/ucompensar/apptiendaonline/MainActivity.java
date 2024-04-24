20089dff0b8694ffb4fe9a7fbf39d34f7be5cfcf
Anyelo Amado <anyelo.amado.dev@outlook.com>
2024-04-24 18:54:36 -0500

    Se crea activity para el menu y se navega a este desde el activity principal si el login es correcto.

--- app/src/main/java/com/ucompensar/apptiendaonline/MainActivity.java
+++ app/src/main/java/com/ucompensar/apptiendaonline/MainActivity.java
@@ -2,13 +2,40 @@ package com.ucompensar.apptiendaonline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnLogin;
+    EditText txtUser;
+    EditText txtPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.button_login);
+        txtUser = findViewById(R.id.text_user);
+        txtPassword = findViewById(R.id.text_password);
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void  onClick(View view){
+
+               if(txtUser.getText().toString().equals("admin") && txtPassword.getText().toString().equals("admin")) {
+                   Toast.makeText(getApplicationContext(), "Iniciando sesión ...",Toast.LENGTH_SHORT).show();
+
+                   Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                   startActivity(intent);
+               }else{
+                   Toast.makeText(getApplicationContext(), "Datos Invalidos ...",Toast.LENGTH_SHORT).show();
+               }
+           }
+        });
     }
 }
---
+++ app/src/main/java/com/ucompensar/apptiendaonline/MenuActivity.java
@@ -0,0 +1,14 @@
+package com.ucompensar.apptiendaonline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
