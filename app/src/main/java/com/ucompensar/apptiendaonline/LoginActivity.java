b6b9a18eaa5bd466730818e1fb3bc91919886fcb
Anyelo Amado <anyelo.amado.dev@outlook.com>
2024-04-24 21:49:28 -0500

    Refactorizacion Activities.

--- app/src/main/java/com/ucompensar/apptiendaonline/LoginActivity.java
+++ app/src/main/java/com/ucompensar/apptiendaonline/LoginActivity.java
@@ -17,7 +17,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         btnLogin = findViewById(R.id.button_login);
         txtUser = findViewById(R.id.text_user);
